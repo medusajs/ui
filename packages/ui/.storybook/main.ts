9ac8e0445541035d836cb72fa28444703a52b87d
Kasper <kasper@medusajs.com>
2023-07-12 17:10:53 +0200

    fix broken storybook version

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-vite",
