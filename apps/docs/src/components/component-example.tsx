a715c8a8f6435bfb3f13c32acfc7910ca35a0956
Rares Capilnar <rares.capilnar@gmail.com>
2023-08-09 20:21:42 +0100

    Add more demos, fix datepicker background, other small tweaks

--- apps/docs/src/components/component-example.tsx
+++ apps/docs/src/components/component-example.tsx
@@ -30,7 +30,7 @@ export function ComponentExample({
   const Code = CodeElement.props.code
 
   return (
-    <div className="relative my-4 flex flex-col space-y-2" {...props}>
+    <div className="relative my-8 flex flex-col space-y-2" {...props}>
       <Tabs defaultValue="preview" className="relative mr-auto w-full">
         <div className="flex flex-col items-center justify-between pb-3">
           <TabsList className="">
@@ -38,15 +38,17 @@ export function ComponentExample({
             <TabsTrigger value="code">Code</TabsTrigger>
           </TabsList>
           <TabsContent value="preview" className="relative">
-            <React.Suspense
-              fallback={
-                <div className="text-ui-fg-muted flex items-center text-sm">
-                  <Spinner className="animate-spin" />
-                </div>
-              }
-            >
-              {Preview}
-            </React.Suspense>
+            <div className="flex w-full items-center justify-center px-10 py-5">
+              <React.Suspense
+                fallback={
+                  <div className="text-ui-fg-muted flex items-center text-sm">
+                    <Spinner className="animate-spin" />
+                  </div>
+                }
+              >
+                {Preview}
+              </React.Suspense>
+            </div>
           </TabsContent>
           <TabsContent value="code" className="relative">
             <CodeBlock code={Code} />
--- apps/docs/src/registry.tsx
+++ apps/docs/src/registry.tsx
@@ -1,9 +1,34 @@
 import * as React from "react"
 
 export const Registry: Record<string, any> = {
+  "avatar-demo": {
+    name: "avatar-demo",
+    component: React.lazy(() => import("@/examples/avatar-demo")),
+    file: "src/examples/avatar-demo.tsx",
+  },
+  "badge-demo": {
+    name: "badge-demo",
+    component: React.lazy(() => import("@/examples/badge-demo")),
+    file: "src/examples/badge-demo.tsx",
+  },
   "button-demo": {
     name: "button-demo",
     component: React.lazy(() => import("@/examples/button-demo")),
     file: "src/examples/button-demo.tsx",
+  },
+  "checkbox-demo": {
+    name: "checkbox-demo",
+    component: React.lazy(() => import("@/examples/checkbox-demo")),
+    file: "src/examples/checkbox-demo.tsx",
+  },
+  "container-demo": {
+    name: "container-demo",
+    component: React.lazy(() => import("@/examples/container-demo")),
+    file: "src/examples/container-demo.tsx",
+  },
+  "date-picker-demo": {
+    name: "date-picker-demo",
+    component: React.lazy(() => import("@/examples/date-picker-demo")),
+    file: "src/examples/date-picker-demo.tsx",
   },
 }
