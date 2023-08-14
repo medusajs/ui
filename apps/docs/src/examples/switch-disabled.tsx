edcf037c0f8985d04bbeca802d5d7ddadc0afbdc
Rares Capilnar <rares.capilnar@gmail.com>
2023-08-14 12:37:05 +0100

    Fix typo

--- apps/docs/src/examples/switch-disabled.tsx
+++ apps/docs/src/examples/switch-disabled.tsx
@@ -3,7 +3,7 @@ import { Label, Switch } from "@medusajs/ui"
 export default function SwitchDisabled() {
   return (
     <div className="flex items-center gap-x-2">
-      <Switch id="manage-inventory-disabled" diasbled={true} />
+      <Switch id="manage-inventory-disabled" disabled={true} />
       <Label htmlFor="manage-inventory-disabled">Manage Inventory</Label>
     </div>
   )
