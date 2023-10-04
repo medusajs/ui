23371ae4c7b82152905e69c78844ee2125966dbc
Kasper <kasper@medusajs.com>
2023-10-04 11:09:43 +0200

    fix: add cursor pointer to dropdown menu items

--- packages/ui/src/components/dropdown-menu/dropdown-menu.tsx
+++ packages/ui/src/components/dropdown-menu/dropdown-menu.tsx
@@ -84,7 +84,7 @@ const Item = React.forwardRef<
   <Primitives.Item
     ref={ref}
     className={clx(
-      "bg-ui-bg-base focus:bg-ui-bg-base-pressed text-ui-fg-base data-[disabled]:text-ui-fg-disabled txt-compact-small relative flex cursor-default select-none items-center rounded-md px-3 py-2 outline-none transition-colors data-[disabled]:pointer-events-none",
+      "bg-ui-bg-base focus:bg-ui-bg-base-pressed text-ui-fg-base data-[disabled]:text-ui-fg-disabled txt-compact-small relative flex select-none items-center rounded-md px-3 py-2 outline-none transition-colors data-[disabled]:pointer-events-none",
       className
     )}
     {...props}
